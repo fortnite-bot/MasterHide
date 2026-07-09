0035166afe2b7f68d32dd8cd52ef52a220355ca5
YL <>
2026-07-09 18:13:23 +0200

    Update project files for signing and include new DLL injector sources

--- MasterHide/MasterHide/Phase2Bridge.h
+++ MasterHide/MasterHide/Phase2Bridge.h
@@ -1,5 +1,13 @@
 #pragma once
 #include <ntddk.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // Called once during DriverEntry to inject the payload DLL into explorer.exe
 NTSTATUS Phase2_TriggerInjection(void);
+
+#ifdef __cplusplus
+}
+#endif
